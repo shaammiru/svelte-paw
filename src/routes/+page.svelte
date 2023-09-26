e4442198a3810ba0012e8f1f4783c767fd3b6b47
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-09-26 16:30:48 +0700

    Create landing page and setup build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import "$lib/home.css"
+</script>
+
+<!-- Header Section -->
+<header>
+  <a href="/" class="logo">Hamami</a>
+
+  <nav>
+    <a href="#hero" class="active">Home</a>
+    <a href="https://syamilh.github.io/syamil-cv/" target="_blank" rel="noopener noreferrer"
+      >About</a
+    >
+    <a href="https://syamilh.github.io/syamil-cv/" target="_blank" rel="noopener noreferrer"
+      >Resume</a
+    >
+    <a href="#contacts">Contacts</a>
+  </nav>
+</header>
+
+<!-- Main Section -->
+<main>
+  <!-- Main - Hero Section -->
+  <section id="hero" class="hero">
+    <div class="hero-container">
+      <div class="hero-main">
+        <h1 class="text">
+          <span>Muhammad Syamil Hamami</span>
+        </h1>
+        <h1>Software Engineer</h1>
+      </div>
+      <div class="hero-button">
+        <a href="#hire" class="hire">Hire Me</a>
+        <a
+          href="https://syamilh.github.io/syamil-cv/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="cv">See CV</a
+        >
+      </div>
+    </div>
+  </section>
+
+  <!-- Main - Cards Section -->
+  <!-- <section class="cards">
+    <div class="home-card card">
+      <div class="icon">🏠</div>
+      <h2>Home</h2>
+      <p>This is home card</p>
+    </div>
+    <div class="about-card card">
+      <div class="icon">ℹ</div>
+      <h2>About</h2>
+      <p>This is about card</p>
+    </div>
+    <div class="resume-card card">
+      <div class="icon">📄</div>
+      <h2>Resume</h2>
+      <p>This is resume card</p>
+    </div>
+    <div class="contacts-card card">
+      <div class="icon">📘</div>
+      <h2>Contacts</h2>
+      <p>This is contacts card</p>
+    </div>
+    <div class="card">
+      <div class="icon">🔑</div>
+      <h2>Lorem</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div class="card">
+      <div class="icon">🔒</div>
+      <h2>Ipsum</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </section> -->
+</main>
+
+<!-- Footer Section -->
+<footer>
+  <div class="footer-container">
+    <p class="license">Released under the MIT License.</p>
+    <p>Copyright © 2023-present by Muhammad Syamil Hamami</p>
+  </div>
+</footer>
